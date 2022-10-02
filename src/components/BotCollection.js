ea2a6edf69b90c12fa23eb2a4382805ae6311d9e
Isaack Mungui <munguiisaack@gmail.com>
2022-10-02 22:54:52 +0300

    Modified props passed to child

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,22 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import BotCard from "./BotCard";
 
 function BotCollection({ bots, addBot, deleteBot }) {
   // Your code here
-  function deleteFromCollection() {
-    fetch(`http://localhost:8002/bots/${bots.id}`, {
-      method: "DELETE",
-    })
-    .then((r) => r.json())
-    .then(() => deleteBot(bots))
-  }
-
   console.log(bots);
   return (
     <div className="ui four column grid">
       <div className="row">
         {bots.map((bot) => (
-          <BotCard key={bot.id} bot={bot} handleAddBot={addBot} handleBotDelete={deleteFromCollection} />
+          <BotCard key={bot.id} bot={bot} handleAddBot={addBot} deleteBot={deleteBot} />
         ))}
       </div>
     </div>
