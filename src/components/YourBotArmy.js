032ef89e99960376034bd56d71a3a71c1913fec4
Isaack Mungui <munguiisaack@gmail.com>
2022-10-02 22:57:18 +0300

    Received props and rendered card component

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,15 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({ botArmy }) {
   //your bot army code here...
-
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {botArmy.map((bot) => (
+            <BotCard  bot={bot} />
+          ))}
         </div>
       </div>
     </div>
