0a32d832426e0b51675a8eca73e8b94a683d1003
Isaack Mungui <munguiisaack@gmail.com>
2022-10-02 10:40:07 +0300

    Added side effect to fecth data and passed props to child component

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [botCollection, setBotCollection] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+    .then((r) => r.json())
+    .then((bots) => {
+      console.log(bots);
+      setBotCollection(bots)
+    })
+  }, [])
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={botCollection} />
     </div>
   )
 }
