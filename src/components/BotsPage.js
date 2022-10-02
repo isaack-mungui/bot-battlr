e6ed661cf36bfd130e4d83545d7c206be5fcbfbf
Isaack Mungui <munguiisaack@gmail.com>
2022-10-02 20:59:52 +0300

    Added function to remove bot from collection

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -6,6 +6,7 @@ import BotCollection from "./BotCollection";
 function BotsPage() {
   //start here with your code for step one
   const [botCollection, setBotCollection] = useState([])
+  const [botArmy, setBotArmy] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:8002/bots")
@@ -16,10 +17,22 @@ function BotsPage() {
     })
   }, [])
 
+  function addBotToArmy(armyBot) {
+    if (!botArmy.find((bot) => bot === armyBot)) {
+      let selectBot = botCollection.find((bot) => bot === armyBot);
+      setBotArmy([...botArmy, selectBot]);
+    }
+  }
+
+  function deleteFromCollection(deletedBot) {
+    const updatedCollection = botCollection.filter((bot) => bot.id !== deletedBot.id)
+    setBotCollection(updatedCollection)
+  }
+
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection bots={botCollection} />
+      <YourBotArmy botArmy={botArmy} />
+      <BotCollection bots={botCollection} addBot={addBotToArmy} deleteBot={deleteFromCollection} />
     </div>
   )
 }
